7823a21a2f906a4bc39e73868a3430f31bebff03
ggthjew4@gmail.com <ggthjew4@gmail.com>
2018-10-30 20:28:41 +0800

    change log config

--- src/main/java/com/serverless/LambdaHandler.java
+++ src/main/java/com/serverless/LambdaHandler.java
@@ -4,13 +4,13 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
-import org.apache.log4j.Logger;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.net.URLDecoder;
 
 public class LambdaHandler implements RequestHandler<S3Event, String> {
 
-    private static final Logger LOG = Logger.getLogger(LambdaHandler.class);
+    private static final Logger LOG = LogManager.getLogger(LambdaHandler.class);
 
     @Override
     public String handleRequest(S3Event s3event, Context context) {
